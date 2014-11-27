a5e0af1ffad0a8c72d0ce5d838130f777916426f
Fedor Nikolayev <fnikolay@ucsc.edu>
2014-11-26 16:38:11 -0800

    Adding module for symboltree
    Adding symboltypes.cpp and symboltypes.h

---
+++ symboltypes.cpp
@@ -0,0 +1,12 @@
+//Ryan Schreiber & Fedor Nikolayev
+
+#include <string>
+using namespace std;
+#include <errno.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wait.h>
+#include <iostream>
---
+++ symboltypes.h
@@ -0,0 +1,9 @@
+//Ryan Schreiber & Fedor Nikolayev
+
+#ifndef __SYMBOLTYPES_H__
+#define __SYMBOLTYPES_H__
+
+#include <stdio.h>
+//Compiler utils that are used through out the compiler
+
+#endif
